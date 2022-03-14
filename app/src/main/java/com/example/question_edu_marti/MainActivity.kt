1d237daf3c20b76e4a6dfaa9bc0cd2c764c53250
deim <deim@d512.lab.deim>
2022-03-14 09:07:26 +0100

    Initial commit

---
+++ app/src/main/java/com/example/question_edu_marti/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.question_edu_marti
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.question_edu_marti.R.layout.activity_questions
+
+class QuestionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activity_questions)
+    }
+}
